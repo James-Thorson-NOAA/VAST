0a04710eebb5f3bf9c37523d27d3436b58d829d1
Jim Thorson <JamesT.esq@gmail.com>
2016-07-19 09:29:25 -0700

    further small changes to dependencies...
    ... now installing TMB and SpatialDeltaGLMM in .onLoad rather than
    DESCRIPTION because it seems more stable

--- R/zzz.R
+++ R/zzz.R
@@ -6,11 +6,15 @@
   packageStartupMessage("Loading package VAST, developed by James Thorson for the Northwest Fisheries Science Center")
   packageStartupMessage("For details and citation guidance, please see http://github.com/james-thorson/VAST/")
   if( !"INLA" %in% installed.packages()[,1] ){
-    message("Installing INLA...")
+    message("Installing package: INLA...")
     install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")  
   }
   if( !"TMB" %in% installed.packages()[,1] ){
-    message("Installing TMB...")
+    message("Installing package: TMB...")
     devtools::install_github("kaskr/adcomp/TMB")
+  }
+  if( !"SpatialDeltaGLMM" %in% installed.packages()[,1] ){
+    message("Installing package: SpatialDeltaGLMM...")
+    devtools::install_github("nwfsc-assess/geostatistical_delta-GLMM")
   }
 }
