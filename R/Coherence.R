26635b035f75e6be3a5355be9053e44f52908bf4
Jim Thorson <JamesT.esq@gmail.com>
2016-07-19 15:43:38 -0700

    added functionality to Coherence...
    ... to allow subsetting of years when calculating coherence index

--- R/Coherence.R
+++ R/Coherence.R
@@ -1,19 +1,22 @@
 
 #' @export
-Coherence = function( obj ){
+Coherence = function( obj, year_set=NULL ){
   # Extract stuff
   report = obj$report()
   tmbdata = obj$env$data
 
+  # Determine defaults
+  if( is.null(year_set)) year_set = 1:tmbdata$n_t
+
   # Derived quantities
-  M_xc = apply( report$D_xct, MARGIN=c(1,2), FUN=mean)
-  S_xc = apply( report$D_xct, MARGIN=c(1,2), FUN=var)
-  Dsum_xt = apply( report$D_xct, MARGIN=c(1,3), FUN=sum)
+  M_xc = apply( report$D_xct[,,year_set,drop=FALSE], MARGIN=c(1,2), FUN=mean)
+  S_xc = apply( report$D_xct[,,year_set,drop=FALSE], MARGIN=c(1,2), FUN=var)
+  Dsum_xt = apply( report$D_xct[,,year_set,drop=FALSE], MARGIN=c(1,3), FUN=sum)
   Msum_x = apply( Dsum_xt, MARGIN=1, FUN=mean)
   Ssum_x = apply( Dsum_xt, MARGIN=1, FUN=var)
-  I_ct = apply( report$D_xct*(tmbdata$a_xl[,1]%o%rep(1,tmbdata$n_c)%o%rep(1,tmbdata$n_t)), MARGIN=c(2,3), FUN=sum)
+  I_ct = apply( report$D_xct[,,year_set,drop=FALSE]*(tmbdata$a_xl[,1]%o%rep(1,tmbdata$n_c)%o%rep(1,length(year_set))), MARGIN=c(2,3), FUN=sum)
   Isum_t = apply( I_ct, MARGIN=2, FUN=sum)
-  Psum_x = apply( Dsum_xt*(tmbdata$a_xl[,1]%o%rep(1,tmbdata$n_t))/(rep(1,tmbdata$n_x)%o%Isum_t), MARGIN=c(1), FUN=mean)
+  Psum_x = apply( Dsum_xt*(tmbdata$a_xl[,1]%o%rep(1,length(year_set)))/(rep(1,tmbdata$n_x)%o%Isum_t), MARGIN=c(1), FUN=mean)
 
   # Synchrony index
   phi_x = Ssum_x / apply(sqrt(S_xc),MARGIN=1,FUN=sum)^2
