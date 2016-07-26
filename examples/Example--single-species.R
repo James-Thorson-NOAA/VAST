0a160162f86dcc2bdf87fc38462568eb24a7de6c
Jim Thorson <JamesT.esq@gmail.com>
2016-07-26 07:16:19 -0700

    small changes in dependencies

--- examples/Example--single-species.R
+++ examples/Example--single-species.R
@@ -26,7 +26,7 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
 # Settings
 ###############
 
-  Data_Set = c("Iceland_cod", "WCGBTS_canary", "GSL_american_plaice", "BC_pacific_cod", "EBS_pollock", "GOA_Pcod", "GOA_pollock", "GB_spring_haddock", "GB_fall_haddock", "SAWC_jacopever", "Sim")[5]
+  Data_Set = c("Iceland_cod", "WCGBTS_canary", "GSL_american_plaice", "BC_pacific_cod", "EBS_pollock", "GOA_Pcod", "GOA_pollock", "GB_spring_haddock", "GB_fall_haddock", "SAWC_jacopever", "Aleutian_islands_POP", "Sim")[5]
   Sim_Settings = list("Species_Set"=1:100, "Nyears"=10, "Nsamp_per_year"=600, "Depth_km"=-1, "Depth_km2"=-1, "Dist_sqrtkm"=0, "SigmaO1"=0.5, "SigmaO2"=0.5, "SigmaE1"=0.5, "SigmaE2"=0.5, "SigmaVY1"=0.05, "Sigma_VY2"=0.05, "Range1"=1000, "Range2"=500, "SigmaM"=1)
   Version = "VAST_v1_8_0"
   Method = c("Grid", "Mesh")[2]
@@ -39,9 +39,12 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
   Kmeans_Config = list( "randomseed"=1, "nstart"=100, "iter.max"=1e3 )     # Samples: Do K-means on trawl locs; Domain: Do K-means on extrapolation grid
 
   # Determine region
-  Region = switch( Data_Set, "Iceland_cod"="Iceland", "WCGBTS_canary"="California_current", "GSL_american_plaice"="Gulf_of_St_Lawrence", "BC_pacific_cod"="British_Columbia", "EBS_pollock"="Eastern_Bering_Sea", "GOA_Pcod"="Gulf_of_Alaska", "GOA_pollock"="Gulf_of_Alaska", "GB_spring_haddock"="Northwest_Atlantic", "GB_fall_haddock"="Northwest_Atlantic", "SAWC_jacopever"="South_Africa", "Sim"="California_current")
+  Region = switch( Data_Set, "Iceland_cod"="Iceland", "WCGBTS_canary"="California_current", "GSL_american_plaice"="Gulf_of_St_Lawrence", "BC_pacific_cod"="British_Columbia", "EBS_pollock"="Eastern_Bering_Sea", "GOA_Pcod"="Gulf_of_Alaska", "GOA_pollock"="Gulf_of_Alaska", "GB_spring_haddock"="Northwest_Atlantic", "GB_fall_haddock"="Northwest_Atlantic", "SAWC_jacopever"="South_Africa", "Aleutian_islands_POP"="Aleutian_Islands", "Sim"="California_current")
 
 # Decide on strata for use when calculating indices
+  if( Data_Set %in% c("GSL_american_plaice","BC_pacific_cod","EBS_pollock","SAWC_jacopever","Chatham_rise_hake","Aleutian_islands_POP")){
+    strata.limits <- data.frame('STRATA'="All_areas")
+  }
   if( Data_Set %in% c("WCGBTS_canary","Sim")){
     # In this case, it will calculate a coastwide index, and also a separate index for each state (although the state lines are approximate)
     strata.limits <- data.frame(
@@ -53,17 +56,6 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
     )
     # Override default settings for vessels
     VesselConfig = c("Vessel"=0, "VesselYear"=1)
-  }
-  if( Data_Set %in% c("GSL_american_plaice")){
-    strata.limits <- data.frame('STRATA'="All_areas")
-  }
-  if( Data_Set %in% c("BC_pacific_cod")){
-    # In this case, will not restrict the extrapolation domain at all while calculating an index
-    strata.limits <- data.frame( 'STRATA'="All_areas")
-  }
-  if( Data_Set %in% c("EBS_pollock")){
-    # In this case, will not restrict the extrapolation domain at all while calculating an index
-    strata.limits <- data.frame( 'STRATA'="All_areas")
   }
   if( Data_Set %in% c("GOA_Pcod","GOA_pollock")){
     # In this case, will calculating an unrestricted index and a separate index restricted to west of -140W
@@ -77,18 +69,9 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
     # For NEFSC indices, strata must be specified as a named list of area codes
     strata.limits = list( 'Georges_Bank'=c(1130, 1140, 1150, 1160, 1170, 1180, 1190, 1200, 1210, 1220, 1230, 1240, 1250, 1290, 1300) )
   }
-  if( Data_Set %in% c("SAWC_jacopever")){
-    strata.limits = data.frame( 'STRATA'="All_areas" )
-  }
-  if( Data_Set %in% c("Iceland_cod")){
-    strata.limits = data.frame( 'STRATA'="All_areas" )
-    # Turn off all spatial, temporal, and spatio-temporal variation in probability of occurrence, because they occur almost everywhere
-    FieldConfig = c("Omega1"=0, "Epsilon1"=0, "Omega2"=1, "Epsilon2"=1)
-    RhoConfig = c("Beta1"=3, "Beta2"=0, "Epsilon1"=0, "Epsilon2"=0) # 0=Off; 1=WhiteNoise; 2=RandomWalk; 3=Constant
-  }
 
   # Save options for future records
-  Record = bundlelist( c("Data_Set","Sim_Settings","Version","Method","grid_size_km","n_x","FieldConfig","RhoConfig","VesselConfig","ObsModel","Kmeans_Config") )
+  Record = ThorsonUtilities::bundlelist( c("Data_Set","Sim_Settings","Version","Method","grid_size_km","n_x","FieldConfig","RhoConfig","VesselConfig","ObsModel","Kmeans_Config") )
   capture.output( Record, file=paste0(DateFile,"Record.txt"))
 
 ################
@@ -104,8 +87,6 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
   if( Data_Set %in% c("BC_pacific_cod")){
     data( BC_pacific_cod_example, package="SpatialDeltaGLMM" )
     Data_Geostat = data.frame( "Catch_KG"=BC_pacific_cod_example[,'PCOD_WEIGHT'], "Year"=BC_pacific_cod_example[,'Year'], "Vessel"="missing", "AreaSwept_km2"=BC_pacific_cod_example[,'TOW.LENGTH..KM.']/100, "Lat"=BC_pacific_cod_example[,'LAT'], "Lon"=BC_pacific_cod_example[,'LON'], "Pass"=0)
-    Data_Geostat = na.omit( Data_Geostat )
-    Data_Geostat$Year = as.numeric( factor(Data_Geostat$Year))
   }
   if( Data_Set %in% c("GSL_american_plaice")){
     data( GSL_american_plaice, package="SpatialDeltaGLMM" )
@@ -119,14 +100,14 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
   if(Data_Set=="GOA_Pcod"){
     data( GOA_pacific_cod, package="SpatialDeltaGLMM" )
     Data_Geostat = data.frame( "Catch_KG"=GOA_pacific_cod[,'catch'], "Year"=GOA_pacific_cod[,'year'], "Vessel"="missing", "AreaSwept_km2"=0.01, "Lat"=GOA_pacific_cod[,'lat'], "Lon"=GOA_pacific_cod[,'lon'], "Pass"=0)
-    # Rename years and keep track of correspondance (for computational speed, given that there's missing years)
-    Data_Geostat$Year = as.numeric( factor(Data_Geostat$Year))
   }
   if(Data_Set=="GOA_pollock"){
     data( GOA_walleye_pollock, package="SpatialDeltaGLMM" )
     Data_Geostat = data.frame( "Catch_KG"=GOA_walleye_pollock[,'catch'], "Year"=GOA_walleye_pollock[,'year'], "Vessel"="missing", "AreaSwept_km2"=0.01, "Lat"=GOA_walleye_pollock[,'lat'], "Lon"=GOA_walleye_pollock[,'lon'], "Pass"=0)
-    # Rename years and keep track of correspondance (for computational speed, given that there's missing years)
-    Data_Geostat$Year = as.numeric( factor(Data_Geostat$Year))
+  }
+  if(Data_Set=="Aleutian_islands_POP"){
+    data( AI_pacific_ocean_perch, package="SpatialDeltaGLMM" )
+    Data_Geostat = data.frame( "Catch_KG"=AI_pacific_ocean_perch[,'cpue..kg.km.2.'], "Year"=AI_pacific_ocean_perch[,'year'], "Vessel"="missing", "AreaSwept_km2"=1, "Lat"=AI_pacific_ocean_perch[,'start.latitude'], "Lon"=AI_pacific_ocean_perch[,'start.longitude'], "Pass"=0)
   }
   if( Data_Set=="GB_spring_haddock"){
     data( georges_bank_haddock_spring, package="SpatialDeltaGLMM" )         # standardized area swept = 0.0112 nm^2 = 0.0112*1.852^2 km^2
@@ -140,9 +121,7 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
   }
   if( Data_Set=="SAWC_jacopever"){
     data( south_africa_westcoast_jacopever, package="SpatialDeltaGLMM" )         # standardized area swept = 0.0112 nm^2 = 0.0112*1.852^2 km^2
-    #Data = read.csv( paste0(getwd(),"/../../examples/archive of data inputs for creation of grid files/South Africa/SAWC_geodata.csv") )
     Data_Geostat = data.frame( "Catch_KG"=south_africa_westcoast_jacopever[,'HELDAC'], "Year"=south_africa_westcoast_jacopever[,'Year'], "Vessel"="missing", "AreaSwept_km2"=south_africa_westcoast_jacopever[,'area_swept_nm2']*1.852^2, "Lat"=south_africa_westcoast_jacopever[,'cen_lat'], "Lon"=south_africa_westcoast_jacopever[,'cen_long'])
-    Data_Geostat$Year = as.numeric( factor(Data_Geostat$Year))
   }
   if(Data_Set=="Sim"){
     Sim_DataSet = Geostat_Sim(Sim_Settings=Sim_Settings, Extrapolation_List=Extrapolation_List, MakePlot=TRUE)
@@ -153,8 +132,8 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
     # WARNING:  This data set has not undergone much evaluation for spatio-temporal analysis
     data( iceland_cod, package="SpatialDeltaGLMM" )
     Data_Geostat = data.frame( "Catch_KG"=iceland_cod[,'Catch_b'], "Year"=iceland_cod[,'year'], "Vessel"=1, "AreaSwept_km2"=iceland_cod[,'towlength'], "Lat"=iceland_cod[,'lat1'], "Lon"=iceland_cod[,'lon1'])
-    Data_Geostat = na.omit( Data_Geostat )
   }
+  Data_Geostat = na.omit( Data_Geostat )
 
 # Get extrapolation data
   if( Region == "California_current" ){
@@ -168,6 +147,9 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
   }
   if( Region == "Gulf_of_Alaska" ){
     Extrapolation_List = SpatialDeltaGLMM::Prepare_Extrapolation_Data_Fn( Region=Region, strata.limits=strata.limits )
+  }
+  if( Region == "Aleutian_Islands" ){
+    Extrapolation_List = Prepare_Extrapolation_Data_Fn( Region=Region, strata.limits=strata.limits )
   }
   if( Region == "Northwest_Atlantic" ){
     Extrapolation_List = SpatialDeltaGLMM::Prepare_Extrapolation_Data_Fn( Region=Region, strata.limits=strata.limits )
@@ -200,28 +182,13 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
   TmbList = Build_TMB_Fn("TmbData"=TmbData, "RunDir"=DateFile, "Version"=Version, "RhoConfig"=RhoConfig, "loc_x"=Spatial_List$loc_x, "TmbDir"=TmbDir)
   Obj = TmbList[["Obj"]]
 
-  # Run first time -- marginal likelihood
-  Start_time = Sys.time()
-  Obj$fn(Obj$par)
-  # Run first time -- gradient with respect to fixed effects
-  Obj$gr(Obj$par)
-
   # Run model
-  for(i in 1:2) Opt = nlminb(start=Obj$env$last.par.best[-Obj$env$random], objective=Obj$fn, gradient=Obj$gr, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], control=list(eval.max=1e4, iter.max=1e4, trace=1))  # , rel.tol=1e-20
-  Opt[["final_diagnostics"]] = data.frame( "Name"=names(Opt$par), "Lwr"=TmbList[["Lower"]], "Est"=Opt$par, "Upr"=TmbList[["Upper"]], "Gradient"=Obj$gr(Opt$par) )
-  Opt[["total_time_to_run"]] = Sys.time() - Start_time
-  Opt[["number_of_coefficients"]] = c("Total"=length(unlist(Obj$env$parameters)), "Fixed"=length(Obj$par), "Random"=length(unlist(Obj$env$parameters))-length(Obj$par) )
-  capture.output( Opt, file=paste0(DateFile,"Opt.txt"))
-    
-  # Reports
-  Report = Obj$report()                                      
-  Sdreport = sdreport(Obj, bias.correct=TRUE)
-  
+  Opt = TMBhelper::Optimize( obj=Obj, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], getsd=TRUE, savedir=DateFile, bias.correct=TRUE )
+  Report = Obj$report()
+
   # Save stuff
-  Save = list("Opt"=Opt, "Report"=Report, "Sdreport"=Sdreport, "ParHat"=Obj$env$parList(Opt$par), "TmbData"=TmbData)
+  Save = list("Opt"=Opt, "Report"=Report, "ParHat"=Obj$env$parList(Opt$par), "TmbData"=TmbData)
   save(Save, file=paste0(DateFile,"Save.RData"))
-  capture.output( Opt, file=paste0(DateFile,"Opt.txt"))
-  capture.output( Sdreport, file=paste0(DateFile,"Sdreport.txt"))
 
 ################
 # Make diagnostic plots
@@ -231,11 +198,13 @@ DateFile = paste(getwd(),'/',Sys.Date(),'_Mesh/',sep='')
   SpatialDeltaGLMM::PlotAniso_Fn( FileName=paste0(DateFile,"Aniso.png"), Report=Report, TmbData=TmbData )
 
   # Plot surface
-  Dim = c( "Nrow"=ceiling(sqrt(TmbData$n_t)), "Ncol"=ceiling(TmbData$n_t/ceiling(sqrt(TmbData$n_t))) )
+  Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
+  Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
+  Dim = c( "Nrow"=ceiling(sqrt(length(Years2Include))), "Ncol"=ceiling(length(Years2Include)/ceiling(sqrt(length(Years2Include)))) )
   par( mfrow=Dim )
-  MapDetails_List = SpatialDeltaGLMM::MapDetails_Fn( "Region"=Region, "NN_Extrap"=Spatial_List$NN_Extrap, "Extrapolation_List"=Extrapolation_List )
-  SpatialDeltaGLMM::PlotResultsOnMap_Fn(plot_set=3, MappingDetails=MapDetails_List[["MappingDetails"]], Report=list("D_xt"=Report$D_xct[,1,]), PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=paste0(DateFile,"Field_"), Year_Set=sort(unique(Data_Geostat[,'Year'])), Rotate=MapDetails_List[["Rotate"]], mfrow=Dim, mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), Cex=MapDetails_List[["Cex"]], cex=1.8)
-                                                                                                                           
+  MapDetails_List = SpatialDeltaGLMM::MapDetails_Fn( "Region"=Region, "NN_Extrap"=Spatial_List$PolygonList$NN_Extrap, "Extrapolation_List"=Extrapolation_List )
+  SpatialDeltaGLMM::PlotResultsOnMap_Fn(plot_set=3, MappingDetails=MapDetails_List[["MappingDetails"]], Report=list("D_xt"=Report$D_xct[,1,]), PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=paste0(DateFile,"Field_"), Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], mfrow=Dim, mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), Cex=MapDetails_List[["Cex"]], cex=1.8)
+
   # Plot index
   SpatialDeltaGLMM::PlotIndex_Fn( DirName=DateFile, TmbData=TmbData, Sdreport=Sdreport, Year_Set=sort(unique(Data_Geostat[,'Year'])), strata_names=strata.limits[,1], use_biascorr=TRUE )
 
