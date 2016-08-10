41b643f423578ac5a006fe900e28770f980fadb2
Jim Thorson <JamesT.esq@gmail.com>
2016-08-09 19:20:51 -0700

    added 'utils' dependency

--- R/zzz.R
+++ R/zzz.R
@@ -7,15 +7,15 @@
   packageStartupMessage("Loading package VAST, developed by James Thorson for the Northwest Fisheries Science Center")
   packageStartupMessage("For details and citation guidance, please see http://github.com/james-thorson/VAST/")
   packageStartupMessage("###########################################################################################")
-  if( !"INLA" %in% installed.packages()[,1] ){
+  if( !"INLA" %in% utils::installed.packages()[,1] ){
     packageStartupMessage("Installing package: INLA...")
-    install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")  
+    utils::install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
   }
-  #if( !"TMB" %in% installed.packages()[,1] ){
+  #if( !"TMB" %in% utils::installed.packages()[,1] ){
   #  packageStartupMessage("Installing package: TMB...")
   #  devtools::install_github("kaskr/adcomp/TMB")
   #}
-  if( !"SpatialDeltaGLMM" %in% installed.packages()[,1] ){
+  if( !"SpatialDeltaGLMM" %in% utils::installed.packages()[,1] ){
     packageStartupMessage("Installing package: SpatialDeltaGLMM...")
     devtools::install_github("nwfsc-assess/geostatistical_delta-GLMM")
   }
