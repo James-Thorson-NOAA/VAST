c962a2c192b615bdb21116f1fc43eab4537d34bf
Jim Thorson <JamesT.esq@gmail.com>
2016-10-11 14:16:52 -0700

    fix bug in Plot_factors...
    ... thanks @pdolder

--- R/Plot_factors.R
+++ R/Plot_factors.R
@@ -9,7 +9,7 @@
 #' @param mapdetails_list output from \code{SpatialDeltaGLMM::MapDetails_Fn}
 #' @param Dim_year Plotting dimension (row,column) for plot of years (default: square with sufficient size for number of years)
 #' @param Dim_species Plotting dimension (row,column) for plot of categories (default: square with sufficient size for number of categories)
-#' @param Dim_species Plotting dimension (row,column) for plot of categories (default: square with sufficient size for number of categories)
+#' @param plotdir directory for saving plots
 
 #' @export
 Plot_factors = function( Report, ParHat, Data, SD, Year_Set=1:dim(Report$D_xct)[3], category_names=1:dim(Report$D_xct)[2],
@@ -31,7 +31,7 @@ Plot_factors = function( Report, ParHat, Data, SD, Year_Set=1:dim(Report$D_xct)[
     if(Par_name %in% c("Omega1","Omega2")) Var_name = paste0(Par_name,"_sc")
 
     # Continue if component is included
-    if( TmbData[["FieldConfig"]][[Par_name]]>0 ){
+    if( Data[["FieldConfig"]][[Par_name]]>0 ){
       # Get covariance #
       # Cov_jj=Cov_List[[paste0("Cov_",tolower(Par_name))]][,,1]; Psi=Report[[Var_name]]; RotationMethod="PCA"; testcutoff=1e-4
       Var_rot = SpatialDFA::Rotate_Fn( Cov_jj=Cov_List[[paste0("Cov_",tolower(Par_name))]][,,'Estimate'], Psi=Report[[Var_name]], RotationMethod="PCA", testcutoff=1e-4 )
@@ -51,7 +51,7 @@ Plot_factors = function( Report, ParHat, Data, SD, Year_Set=1:dim(Report$D_xct)[
 
         # Plot average factors across years
         Mat_sc = apply(Var_rot$Psi_rot, MARGIN=1:2, FUN=mean)
-        SpatialDeltaGLMM:::PlotMap_Fn( MappingDetails=mapdetails_list[["MappingDetails"]], Mat=Mat_sc, PlotDF=mapdetails_list[["PlotDF"]], MapSizeRatio=mapdetails_list[["MapSizeRatio"]], Xlim=mapdetails_list[["Xlim"]], Ylim=mapdetails_list[["Ylim"]], FileName=paste0(plotdir,"Factor_maps--",Par_name), Year_Set=paste0("Factor_",1:length(category_names)), Rotate=mapdetails_list[["Rotate"]], zone=mapdetails_list[["Zone"]], mar=c(0,0,2,0), oma=c(2.5,2.5,0,0), Cex=0.01, mfrow=Dim_species, pch=20, Legend=mapdetails_list[["Legend"]], zone=mapdetails_list[["Zone"]], plot_legend_fig=FALSE)
+        SpatialDeltaGLMM:::PlotMap_Fn( MappingDetails=mapdetails_list[["MappingDetails"]], Mat=Mat_sc, PlotDF=mapdetails_list[["PlotDF"]], MapSizeRatio=mapdetails_list[["MapSizeRatio"]], Xlim=mapdetails_list[["Xlim"]], Ylim=mapdetails_list[["Ylim"]], FileName=paste0(plotdir,"Factor_maps--",Par_name), Year_Set=paste0("Factor_",1:length(category_names)), Rotate=mapdetails_list[["Rotate"]], zone=mapdetails_list[["Zone"]], mar=c(0,0,2,0), oma=c(2.5,2.5,0,0), Cex=0.01, mfrow=Dim_species, pch=20, Legend=mapdetails_list[["Legend"]], plot_legend_fig=FALSE)
       }
     }  #
   }
