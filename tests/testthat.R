6a4f7877a2376a65b6fd1e025e343356f0b4c4b0
Jim Thorson <JamesT.esq@gmail.com>
2016-12-19 16:14:12 -0800

    update VAST ...
    .... to allow `Method="Spherical_mesh"`

--- tests/testthat.R
+++ tests/testthat.R
@@ -13,10 +13,14 @@ multispecies_example_path <- system.file("extdata", package="VAST")
 #setwd(system.file("tests", package="VAST"))
 testthat::test_check("VAST")
 
+################
 # Local testing
-  # Use local path
-  #multispecies_example_path <- "C:/Users/James.Thorson/Desktop/Project_git/VAST/inst/extdata/"
+################
 
-  # Run from local directory
-  #testthat::test_dir( "C:/Users/James.Thorson/Desktop/Project_git/VAST/tests/testthat/", reporter="check" )
-  #testthat::test_dir( "/media/sf_c/Users/jim/Desktop/Project_git/VAST/tests/testthat/", reporter="check" )
+# Use local path
+#singlespecies_example_path <- "C:/Users/James.Thorson/Desktop/Project_git/geostatistical_delta-GLMM/inst/extdata/"
+#multispecies_example_path <- "C:/Users/James.Thorson/Desktop/Project_git/VAST/inst/extdata/"
+
+# Run from local directory
+#testthat::test_dir( "C:/Users/James.Thorson/Desktop/Project_git/VAST/tests/testthat/", reporter="check" )
+#testthat::test_dir( "/media/sf_c/Users/jim/Desktop/Project_git/VAST/tests/testthat/", reporter="check" )
