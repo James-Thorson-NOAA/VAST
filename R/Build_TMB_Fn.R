0730c5e3eb9828305412759a67a51705d0635230
Jim Thorson <JamesT.esq@gmail.com>
2017-07-09 15:27:21 -0700

    List fixed and random effects in clear way...
    ... using new function `ThorsonUtilities::list_parameters`

--- R/Build_TMB_Fn.R
+++ R/Build_TMB_Fn.R
@@ -146,8 +146,7 @@ function( TmbData, Version, Q_Config=TRUE, CovConfig=TRUE,
   Obj$env$inner.control$grad.tol <- c(1e-8,1e-12,1e-15)[ConvergeTol] # # Default : 1e-8  # Maximum gradient limit inner optimization
 
   # Print number of parameters
-  message("Number of fixed and random effects:")
-  print( table(names(Obj$env$last.par)) )
+  ThorsonUtilities::list_parameters( Obj )
 
   # Return stuff
   Return = list("Obj"=Obj, "Upper"=Bounds[,'Upper'], "Lower"=Bounds[,'Lower'], "Parameters"=Parameters, "Map"=Map, "Random"=Random)
