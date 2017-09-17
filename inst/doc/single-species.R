39671c2db28b60461e854b3c92710df91635778e
Jim Thorson <JamesT.esq@gmail.com>
2017-09-17 14:30:25 -0400

    delete old files

--- inst/doc/single-species.R
+++
@@ -1,9 +0,0 @@
-## ----set_options, echo=FALSE, message=FALSE, warning=FALSE----
-# Width should apply to tidy
-# digits hopefully affects number of digits when using print
-options(width=50, width.cutoff=50, digits = 3) 
-install.packages("pander", repos="http://cran.us.r-project.org")
-
-## ----setup, include=FALSE-----------------------
-knitr::opts_chunk$set(cache=TRUE, cache.path="cache_single/")
-
