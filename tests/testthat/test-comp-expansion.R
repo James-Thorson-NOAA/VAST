a2423de73e53f39d9b95fa27203485fa14993d48
Jim Thorson <JamesT.esq@gmail.com>
2019-01-04 21:43:46 -0800

    small fix to lingcod testthat...
    ... but its still not working with V7.0.0

--- tests/testthat/test-comp-expansion.R
+++ tests/testthat/test-comp-expansion.R
@@ -24,7 +24,7 @@ test_that("Male lingcod compositional expansion is working ", {
   on.exit( dyn.unload(paste0(test_path,"/",TMB::dynlib(Version_VAST))), add=TRUE )
   Opt = TMBhelper::Optimize( obj=TmbList[["Obj"]], getsd=TRUE, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], newtonsteps=1 )  # , rel.tol=1e-20
   # Comparisons
-  Par1 = Opt$par[names(Opt$par)%in%c("ln_H_input","beta1_ct","logkappa1","beta2_ct","logkappa1","logSigmaM")]
+  Par1 = Opt$par[names(Opt$par)%in%c("ln_H_input","beta1_ct","beta1_tf","logkappa1","beta2_ct","beta2_tf","logkappa1","logSigmaM")]
   Par2 = opt$par[names(opt$par)%in%c("ln_H_input","beta1_ct","logkappa1","beta2_ct","logkappa1","logSigmaM")]
   expect_equal( as.vector(Par1), as.vector(Par2), tolerance=1e-3 )
   # Get expanded composition estimates and input sample sizes
