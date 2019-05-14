7bb8d27b69cf0e5c896365f081830b2874ffe08c
James.Thorson <James.Thorson>
2019-05-14 08:33:18 -0700

    small update to check_fit

--- R/check_fit.R
+++ R/check_fit.R
@@ -3,19 +3,20 @@
 #' \code{check_fit} checks bounds and throws an informative message if any look bad
 #'
 #' @param parameter_estimates output from \code{TMBhelper::fit_tmb}
+#' @param check_bounds Boolean stating whether to check bounds as well as other issues
 #' @param quiet Boolean stating whether to print warnings to terminal
-#' @return Return Error code (TRUE is good; FALSE is bad)
+#' @return Did an automated check find an obvious problem code (TRUE is bad; FALSE is good)
 
 #' @export
-check_fit = function( parameter_estimates, quiet=FALSE ){
+check_fit = function( parameter_estimates, check_bounds=FALSE, quiet=FALSE ){
 
   # Initialize code for good model
-  Code = TRUE
+  problem_found = FALSE
 
   # Check for informative bounds
   On_bounds = ifelse( parameter_estimates$par<(parameter_estimates$diagnostics[,'Lower']+0.0001) | parameter_estimates$par>(parameter_estimates$diagnostics[,'Upper']-0.0001), TRUE, FALSE )
   if( any(On_bounds) ){
-    Code = FALSE
+    problem_found = TRUE
     if(quiet==FALSE){
       message("\nCheck bounds for the following parameters:")
       print( parameter_estimates$diagnostics[which(On_bounds),] )
@@ -25,7 +26,7 @@ check_fit = function( parameter_estimates, quiet=FALSE ){
   # Check for stuff at zero
   At_zero = ifelse( abs(parameter_estimates$par) < 0.0001, TRUE, FALSE )
   if( any(At_zero) ){
-    Code = FALSE
+    problem_found = TRUE
     if(quiet==FALSE){
       message("\nThe following parameters appear to be approaching zero:")
       print( parameter_estimates$diagnostics[which(At_zero),] )
@@ -37,8 +38,8 @@ check_fit = function( parameter_estimates, quiet=FALSE ){
 
   # Check bad gradients
   Bad_gradient = ifelse( abs(parameter_estimates$diagnostics[,'final_gradient']) > 0.001, TRUE, FALSE )
-  if( any(Bad_gradient) ){
-    Code = FALSE
+  if( check_bounds==TRUE && any(Bad_gradient) ){
+    problem_found = TRUE
     if(quiet==FALSE){
       message("\nThe following parameters has a bad final gradient:")
       print( parameter_estimates$diagnostics[which(Bad_gradient),] )
@@ -46,6 +47,6 @@ check_fit = function( parameter_estimates, quiet=FALSE ){
   }
 
   # Return
-  return( invisible(Code) )
+  return( invisible(problem_found) )
 }
 
