5526040f166b199606be094dd6820b18a441c27d
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2019-06-09 15:46:40 -0700

    Convert test-comp-expansion...
    ... to use `fit_model` interface

--- tests/testthat/test-comp-expansion.R
+++ tests/testthat/test-comp-expansion.R
@@ -4,29 +4,38 @@ context("Testing examples")
 # Eastern Bering Sea pollcok
 test_that("Male lingcod compositional expansion is working ", {
   skip_on_travis()
+
+  # Prepping
   test_path = file.path(multispecies_example_path,"Lingcod_comp_expansion")
   load( file=file.path(test_path,"Data_Geostat.RData") )
   load( file.path(test_path,"opt.RData") )
   load( file.path(test_path,"Record.RData") )
   attach(Record)
   on.exit( detach(Record) )
+
+  # Make settings
+  settings = make_settings( n_x=n_x, Region="California_current" )
+
+  # Change settings from defaults
+  settings$ObsModel = ObsModel_Est
+  settings$use_anisotropy = Aniso
+  settings$fine_scale = FALSE
+
   # Run model
-  Extrapolation_List = make_extrapolation_info( Region="California_current", strata.limits=strata.limits )
-  Spatial_List = make_spatial_info( grid_size_km=grid_size_km, n_x=n_x, Method=Method, Lon=Data_Geostat[,'BEST_LON_DD'], Lat=Data_Geostat[,'BEST_LAT_DD'], Extrapolation_List=Extrapolation_List, DirPath=test_path )
-  TmbData = make_data("Options"=list("treat_nonencounter_as_zero"=TRUE), "Version"=Version_VAST, "OverdispersionConfig"=OverdispersionConfig, "FieldConfig"=FieldConfig, "RhoConfig"=RhoConfig, "ObsModel"=ObsModel_Est, "c_i"=as.numeric(Data_Geostat[,'Length_bin'])-1, "b_i"=Data_Geostat[,'First_stage_expanded_numbers'], "a_i"=Data_Geostat[,'AreaSwept_km2'], "v_i"=rep(0,nrow(Data_Geostat)), "t_i"=Data_Geostat[,'Year'], "spatial_list"=Spatial_List, "Aniso"=Aniso )
-  # Build object
-  TmbList = make_model("build_model"=TRUE, "TmbData"=TmbData, "RunDir"=test_path, "Version"=Version_VAST, "RhoConfig"=RhoConfig, "loc_x"=Spatial_List$loc_x, "Npool"=Npool)
-  # Optimize
-  on.exit( dyn.unload(paste0(test_path,"/",TMB::dynlib(Version_VAST))), add=TRUE )
-  Opt = TMBhelper::Optimize( obj=TmbList[["Obj"]], getsd=FALSE, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], newtonsteps=1 )  # , rel.tol=1e-20
+  fit = fit_model( "settings"=settings, "Lat_i"=Data_Geostat[,'BEST_LAT_DD'], "Lon_i"=Data_Geostat[,'BEST_LON_DD'],
+    "c_i"=as.numeric(Data_Geostat[,'Length_bin'])-1, "b_i"=Data_Geostat[,'First_stage_expanded_numbers'], "a_i"=Data_Geostat[,'AreaSwept_km2'],
+    "v_i"=rep(0,nrow(Data_Geostat)), "t_i"=Data_Geostat[,'Year'], model_args=list("Npool"=Npool), optimize_args=list("getsd"=FALSE,"savedir"=NULL),
+    newtonsteps=1, test_fit=FALSE, working_dir=test_path  )
+
   # Comparisons
-  Par1 = Opt$par[names(Opt$par)%in%c("ln_H_input","beta1_ct","beta1_ft","logkappa1","beta2_ct","beta2_ft","logkappa1","logSigmaM")]
+  Par1 = fit$parameter_estimates$par[names(fit$parameter_estimates$par)%in%c("ln_H_input","beta1_ct","beta1_ft","logkappa1","beta2_ct","beta2_ft","logkappa1","logSigmaM")]
   Par2 = opt$par[names(opt$par)%in%c("ln_H_input","beta1_ct","logkappa1","beta2_ct","logkappa1","logSigmaM")]
   expect_equal( as.vector(Par1), as.vector(Par2), tolerance=1e-3 )
-  expect_equal( opt$objective, Opt$objective, tolerance=1e-3 )
+  expect_equal( opt$objective, fit$parameter_estimates$objective, tolerance=1e-3 )
+
   # Get expanded composition estimates and input sample sizes
-  #Opt = TMBhelper::Optimize( obj=TmbList[["Obj"]], startpar=Opt$par, getsd=TRUE, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], newtonsteps=0 )
-  #Index = plot_biomass_index( TmbData=TmbData, Sdreport=Opt$SD, DirName=test_path )
-  #calculate_proportion( TmbData=TmbData, Index=Index, DirName=test_path, Year_Set=unique(Data_Geostat$Year) )
+  #Opt = TMBhelper::fit_tmb( obj=fit$tmb_list$Obj, startpar=fit$parameter_estimates$par, getsd=TRUE, lower=fit$tmb_list$Lower, upper=fit$tmb_list$Upper, newtonsteps=0 )
+  #Index = plot_biomass_index( TmbData=fit$data_list, Sdreport=fit$parameter_estimates$SD, DirName=test_path )
+  #calculate_proportion( TmbData=fit$data_list, Index=Index, DirName=test_path, Year_Set=fit$year_labels )
 })
 
