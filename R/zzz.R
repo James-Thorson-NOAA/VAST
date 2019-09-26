0200611d01eae45c3b785cf9d9cd79e4ac1234d1
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2019-09-25 16:57:17 -0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -13,7 +13,14 @@
   if( !"INLA" %in% utils::installed.packages()[,1] ){
     packageStartupMessage("Installing package: INLA...")
     #utils::install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
-    utils::install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+
+    # Over-ride default install for R 3.5.0 through R 3.5.3
+    Rvers = numeric_version(paste0(R.version[6:7],collapse="."))
+    if( Rvers<numeric_version("3.6.0") & Rvers>numeric_version("3.5.0") ){
+      utils::install.packages( "https://inla.r-inla-download.org/R/stable/bin/windows/contrib/3.5/INLA_18.07.12.zip" )
+    }else{
+      utils::install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+    }
   }
 
   # Load `FishStatsUtils` via .onAttach because importFrom wasn't working
