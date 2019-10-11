e62994f693a73be10ced78b63cb80fe7a4ec8b93
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2019-10-11 14:37:18 -0700

    Update dependency...
    ... to use FishStatsUtils >= 2.3.2 to avoid using FishStatsUtils versions with a bug in make_covariates(.)

--- R/zzz.R
+++ R/zzz.R
@@ -25,9 +25,9 @@
 
   # Load `FishStatsUtils` via .onAttach because importFrom wasn't working
   # Also requries moving FishStatsUtils to SUGGESTS, so that it doesn't isntall main branch
-  if( !"FishStatsUtils" %in% utils::installed.packages()[,1] || utils::packageVersion("FishStatsUtils") < numeric_version("2.3.0") ){
-    packageStartupMessage("Updating package FishStatsUtils because previously using version < 2.3.0")
-    devtools::install_github("james-thorson/FishStatsUtils", ref="2.3.0")
+  if( !"FishStatsUtils" %in% utils::installed.packages()[,1] || utils::packageVersion("FishStatsUtils") < numeric_version("2.3.2") ){
+    packageStartupMessage("Updating package FishStatsUtils because previously using version < 2.3.2")
+    devtools::install_github("james-thorson/FishStatsUtils", ref="2.3.2")
   }
   packageStartupMessage( "Loading package `FishStatsUtils` version ", packageVersion("FishStatsUtils") )
   library(FishStatsUtils)
