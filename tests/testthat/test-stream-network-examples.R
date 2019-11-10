b99124fb9b203b19e7b196994a34dbb6882648e7
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2019-11-09 17:17:48 -0800

    Update test-stream-network-examples.R

--- tests/testthat/test-stream-network-examples.R
+++ tests/testthat/test-stream-network-examples.R
@@ -7,6 +7,7 @@ test_that("Stream network example is working ", {
   test_path = file.path(multispecies_example_path,"Stream_network")
   load( file.path(test_path,"parameter_estimates.RData") )
   load( file.path(test_path, "settings.RData"))
+  settings$Version = FishStatsUtils::get_latest_version()
   attach(settings)
   on.exit( detach(settings) )
 
