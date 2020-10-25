eba0bab63f528e56eed5ecc5cebbc79f1b9f5cc5
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2020-10-24 19:11:32 -0700

    Add integrated-test for covariate effects...
    .. when using also using a smoother via mgcv::gam.  Works well with Tweedie model, and OK for 2nd linear predictor of conventional delta-model.

--- tests/testthat/test-Tweedie-against-mgcv.R
+++ tests/testthat/test-Tweedie-against-mgcv.R
@@ -3,11 +3,11 @@
 # And example see: https://github.com/ss3sim/ss3sim/tree/master/tests/testthat
 context("Testing examples")
 
-# Eastern Bering Sea pollcok
+# Tweedie distribution
 test_that("Tweedie gives identical results to mgcv::gam(.) ", {
   skip_on_travis()
+  library(mgcv)
 
-  #library(mgcv)
   #library(tweedie)  # Installed from locally from tweedie_2.3.2.tar.gz here: https://cran.r-project.org/web/packages/tweedie/index.html
   # Simulate
   n_obs = 50
@@ -23,16 +23,10 @@ test_that("Tweedie gives identical results to mgcv::gam(.) ", {
   t_i = rep(1, n_obs)
 
   # Fit using GAM
-  mgcv_gam = mgcv::gam( d_i ~ 1, family=tw )
+  mgcv_gam = gam( d_i ~ 1, family=tw )
   p = 1 + plogis(mgcv_gam$family$getTheta())
-  #mgcv_gam$scale
-
-  # Load package
-  library(VAST)
 
   # load data set
-  # see `?load_example` for list of stocks with example data
-  # that are installed automatically with `FishStatsUtils`.
   example = load_example( data_set="EBS_pollock" )
 
   # Make settings (turning off bias.correct to save time for example)
@@ -63,3 +57,104 @@ test_that("Tweedie gives identical results to mgcv::gam(.) ", {
   # Comparison -- scale
   expect_equal( as.numeric(exp(fit$ParHat$beta1_ft)), as.numeric(mgcv_gam$scale), tolerance=0.05 )
 })
+
+# Eastern Bering Sea pollcok
+test_that("Covariate effects when using a smoother gives identical results to mgcv::gam(.) ", {
+  skip_on_travis()
+  library(mgcv)
+
+  # Simulate
+  data( EBS_pollock_data, package="FishStatsUtils" )
+  pollock_data = EBS_pollock_data[ which(EBS_pollock_data$waterTmpC > -999), ]
+
+  # load data set
+  example = load_example( data_set="EBS_pollock" )
+  covariate_data = data.frame( "Lat"=pollock_data$lat, "Lon"=pollock_data$long, "Year"=pollock_data$year, "Temp"=pollock_data$waterTmpC)
+
+  ##################
+  # Tweedie
+  ##################
+
+  # Make settings (turning off bias.correct to save time for example)
+  settings = make_settings( n_x = 100,
+    Region = "Eastern_Bering_Sea",
+    purpose = "index2",
+    ObsModel = c(10,2),
+    max_cells = Inf,
+    fine_scale = FALSE,
+    use_anisotropy = FALSE )
+  settings$FieldConfig[c('Omega','Epsilon'),c('Component_1','Component_2')] = 0
+  settings$FieldConfig['Omega','Component_2'] = 1
+  settings$RhoConfig['Beta1'] = 3
+
+  # Run model
+  fit_tweedie = fit_model( settings = settings,
+    Lat_i = pollock_data[,'lat'],
+    Lon_i = pollock_data[,'long'],
+    t_i = pollock_data[,'year'],
+    b_i = pollock_data[,'catch'],
+    a_i = rep(0.01, nrow(pollock_data) ),
+    covariate_data = covariate_data,
+    X2_formula = ~ Temp + I(Temp^2),
+    working_dir = multispecies_example_path,
+    getsd = FALSE,
+    Use_REML = TRUE )
+  p_hat = 1 + plogis( fit_tweedie$ParHat$logSigmaM[1,1] )
+
+  # Fit using GAM
+  x_i = ( fit$spatial_list$A_is%*%(1:ncol(fit$spatial_list$A_is)) )[,1]
+  Data = cbind(pollock_data, fit$spatial_list$loc_i, "Area_km2"=0.01, "pres"=ifelse(pollock_data$catch==0,0,1),
+    Temp_i=fit$data_list$X2_ip[,1], "east_i"=fit$spatial_list$loc_x[x_i,"E_km"], "north_i"=fit$spatial_list$loc_x[x_i,"N_km"] )
+  mgcv_gam = gam( catch ~ 0 + factor(year) + Temp_i + I(Temp_i^2) + s(east_i,north_i,bs="gp"), family=tw,
+    offset=log(Area_km2), data=Data, method="ML" )
+  p = 1 + plogis(mgcv_gam$family$getTheta())
+  #mgcv_gam$scale
+
+  # slopes
+  expect_equal( as.numeric(fit_tweedie$ParHat$gamma2_cp), as.numeric(summary(gam2)$p.coeff[c('Temp_i','I(Temp_i^2)')]), tolerance=0.01 )
+  expect_equal( as.numeric(p_hat), as.numeric(p), tolerance=0.01 )
+
+  ##################
+  # Conventional delta-lognormal
+  #  1.  Doesn't work well for 1st linear predictor for reasons I don't understand
+  ##################
+
+  # Make settings (turning off bias.correct to save time for example)
+  settings = make_settings( n_x = 100,
+    Region = "Eastern_Bering_Sea",
+    purpose = "index2",
+    ObsModel = c(1,0),
+    max_cells = Inf,
+    fine_scale = FALSE,
+    use_anisotropy = FALSE )
+  settings$FieldConfig['Epsilon',c('Component_1','Component_2')] = 0
+  #settings$FieldConfig['Omega',c('Component_1','Component_2')] = 0
+
+  # Run model
+  fit = fit_model( settings = settings,
+    Lat_i = pollock_data[,'lat'],
+    Lon_i = pollock_data[,'long'],
+    t_i = pollock_data[,'year'],
+    b_i = pollock_data[,'catch'],
+    a_i = rep(0.01, nrow(pollock_data) ),
+    covariate_data = covariate_data,
+    X1_formula = ~ Temp + I(Temp^2),
+    X2_formula = ~ Temp + I(Temp^2),
+    working_dir = multispecies_example_path,
+    getsd = FALSE,
+    Use_REML = TRUE )
+
+  # Fit using GAM
+  x_i = ( fit$spatial_list$A_is%*%(1:ncol(fit$spatial_list$A_is)) )[,1]
+  Data = cbind(pollock_data, fit$spatial_list$loc_i, "Area_km2"=0.01, "pres"=ifelse(pollock_data$catch==0,0,1),
+    Temp_i=fit$data_list$X1_ip[,1], "east_i"=fit$spatial_list$loc_x[x_i,"E_km"], "north_i"=fit$spatial_list$loc_x[x_i,"N_km"] )
+  gam1 = gam( pres ~ 0 + factor(year) + Temp_i + I(Temp_i^2) + s(east_i,north_i,bs='gp'),
+    family=binomial(link="logit"), data=Data, method="ML" )  # + te(lat,long,bs="gp") + te(lat,long,by=Year,bs="gp")
+  gam2 = gam( log(catch) ~ 0 + factor(year)+ Temp_i + I(Temp_i^2) + s(east_i,north_i,bs='gp'),
+    family=gaussian, offset=log(Area_km2), data=Data[which(Data$catch>0),], method="ML" )
+
+  # slopes
+  expect_equal( as.numeric(fit$ParHat$gamma1_cp), as.numeric(summary(gam1)$p.coeff[c('Temp_i','I(Temp_i^2)')]), tolerance=0.1 )
+  expect_equal( as.numeric(fit$ParHat$gamma2_cp), as.numeric(summary(gam2)$p.coeff[c('Temp_i','I(Temp_i^2)')]), tolerance=0.1 )
+})
+
