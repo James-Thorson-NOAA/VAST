36fcc89a515d3667e4a9f7f97962d21a3a4ca813
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2021-01-06 16:51:46 -0800

    small updates to effects

--- R/effects.R
+++ R/effects.R
@@ -14,8 +14,11 @@ Effect.fit_model = function (focal.predictors, mod, which_formula="X1", ...) {
   if( !all(c("covariate_data_full","catchability_data_full") %in% ls(.GlobalEnv)) ){
     stop("Please load `covariate_data_full` and `catchability_data_full` into global memory")
   }
+  if( !requireNamespace("effects") ){
+    stop("please install the effects package")
+  }
 
-  # Basic
+  # Identify formula-speific stuff
   if( which_formula=="X1" ){
     formula_orig = mod$X1_formula
     parname = "gamma1_cp"
@@ -24,11 +27,19 @@ Effect.fit_model = function (focal.predictors, mod, which_formula="X1", ...) {
     formula_orig = mod$X2_formula
     parname = "gamma2_cp"
     mod$call = mod$effects$call_X2
+  else if( which_formula=="Q1" ){
+    formula_orig = mod$Q1_formula
+    parname = "lambda1_k"
+    mod$call = mod$effects$call_Q1
+  else if( which_formula=="Q2" ){
+    formula_orig = mod$Q2_formula
+    parname = "lambda2_k"
+    mod$call = mod$effects$call_Q2
   }else{
-    stop("Other options are not implemented")
+    stop("Check `which_formula` input")
   }
 
-  # Extract parameters
+  # Extract parameters / covariance
   whichnum = which(names(mod$parameter_estimates$par)==parname)
   mod$parhat = mod$parameter_estimates$par[whichnum]
     mod$covhat = mod$parameter_estimates$SD$cov.fixed[whichnum,whichnum]
@@ -54,26 +65,31 @@ Effect.fit_model = function (focal.predictors, mod, which_formula="X1", ...) {
   family.fit_model = function(x,...) x$family
   vcov.fit_model = function(x,...) x$vcov
 
-  # Do stuff
-  fam = mod$family
-  ## dummy functions to make Effect.default work
+  # dummy functions to make Effect.default work
   dummyfuns = list(variance = function(mu) mu,
                     initialize = expression(mustart = y + 0.1),
                     dev.resids = function(...) poisson()$dev.res(...) )
+
+  # Replace family (for reasons I don't really understand)
+  fam = mod$family
   for( i in names(dummyfuns) ){
     if( is.null(fam[[i]]) ) fam[[i]] = dummyfuns[[i]]
   }
-  ## allow calculation of effects ...
+
+  # allow calculation of effects ...
   if (length(formals(fam$variance))>1) {
     warning("overriding variance function for effects: computed variances may be incorrect")
     fam$variance = dummyfuns$variance
   }
+
+  # Bundle arguments
   args = list(call = mod$call,
                coefficients = mod$coefficients,
                vcov = mod$vcov,
                family = fam,
                formula = formula_full)
-  if(!requireNamespace("effects")) stop("please install the effects package")
+
+  # Do call
   effects::Effect.default(focal.predictors,
     mod,
     ...,
