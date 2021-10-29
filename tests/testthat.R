2aadbe316123dd5a30b90b4a3c19b374a7b0fcdf
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2021-10-29 12:21:59 -0700

    small updates

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,8 +2,13 @@
 # devtools::install_github("james-thorson/FishStatsUtils", ref="development")
 # devtools::install_github("james-thorson/VAST", ref="development")
 # devtools::install_local("C:/Users/James.Thorson/Desktop/Git/TMB_contrib_R/TMBhelper", force=TRUE, dep=FALSE)
+
+# devtools::document("C:/Users/James.Thorson/Desktop/Git/FishStatsUtils")
 # devtools::install_local("C:/Users/James.Thorson/Desktop/Git/FishStatsUtils", force=TRUE, dep=FALSE)
+# devtools::document("C:/Users/James.Thorson/Desktop/Git/VAST")
 # devtools::install_local("C:/Users/James.Thorson/Desktop/Git/VAST", force=TRUE, dep=FALSE)
+# devtools::document("C:/Users/James.Thorson/Desktop/Git/FishStatsUtils")
+
 
 library(testthat)
 library(VAST)
