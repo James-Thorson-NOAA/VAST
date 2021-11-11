4dcc779c0b04f2d036e06eb1cdac7d5ffd44d85e
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2021-11-11 14:12:44 -0800

    small updates into new dev branch

--- R/zzz.R
+++ R/zzz.R
@@ -28,9 +28,9 @@
   # Also requries moving FishStatsUtils to SUGGESTS, so that it
   # doesn't isntall main branch
 
-  if( !"FishStatsUtils" %in% utils::installed.packages()[,1] || utils::packageVersion("FishStatsUtils") < numeric_version("2.10.2") ){
-    packageStartupMessage("Updating package FishStatsUtils because previously using version < 2.10.2")
-    devtools::install_github("james-thorson/FishStatsUtils", ref="2.10.2")
+  if( !"FishStatsUtils" %in% utils::installed.packages()[,1] || utils::packageVersion("FishStatsUtils") < numeric_version("2.11.0") ){
+    packageStartupMessage("Updating package FishStatsUtils because previously using version < 2.11.0")
+    devtools::install_github("james-thorson/FishStatsUtils", ref="dev")
     ## devtools::install_github("james-thorson/FishStatsUtils", ref='development', INSTALL_opts="--no-staged-install")
   }
   packageStartupMessage( "Loading package `FishStatsUtils` version ", packageVersion("FishStatsUtils") )
