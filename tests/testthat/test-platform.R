70f3540b930fb568915c1b372adff05df8de7a7d
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2021-12-10 07:51:14 -0800

    add back "test-platform" CI check

--- tests/testthat/test-platform.R
+++ tests/testthat/test-platform.R
@@ -4,7 +4,8 @@ context("Testing cross platform and R version compatibility")
 
 # Eastern Bering Sea pollcok
 test_that("Eastern Bering Sea pollock is working ", {
-  skip_on_ci()
+  #skip_on_ci()
+
   ## Prep really simple example using built-in data set, adapted
   ## from simple example on wiki
   example <- load_example( data_set="EBS_pollock" )
