bc513e4b47bf027ad6ad4ecb14d2358947b6409c
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2023-04-28 10:00:57 -0800

    add error check for project_model

--- R/project_model.R
+++ R/project_model.R
@@ -93,6 +93,9 @@ function( x,
   if( any(x$data_list$RhoConfig[c("Beta1","Beta2")] %in% c(0)) ){
     stop("`project_model` is currently designed to work only with temporally varying or constant beta terms")
   }
+  if( FishStatsUtils::convert_version_name(x$settings$Version) < FishStatsUtils::convert_version_name("VAST_v14_0_1") ){
+    stop("`project_model` requires version >= `VAST_v14_0_1`")
+  }
 
   ##############
   # Step 1: Generate uncertainty in historical period
