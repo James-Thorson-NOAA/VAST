0cbad02ec0a52a31a3a99c38361a9b6735a16f24
Jim Thorson <James.T.Thorson@gmail.com>
2025-02-08 10:22:42 -0800

    fixing Imports

--- scratch/test_search.R
+++ scratch/test_search.R
@@ -2,13 +2,15 @@
 
 setwd( R'(C:\Users\James.Thorson\Desktop\Git\VAST\R)' )
 files = list.files()
+search_terms = c("ThorsonUtilities", "FishStatsUtils", "TMBhelper")
 
+for(j in seq_along(search_terms)){
 for(i in seq_along(files)){
-  read = scan( files[i], what="character" )
-  if(length(grep( "jpeg", read ))){
-    stop( "Check ", files[i] )
+  read = scan( files[i], what="character", quiet = TRUE )
+  if(length(grep( search_terms[j], read ))){
+    stop( "Check ", files[i], " for ", search_terms[j] )
   }
-}
+}}
 
 setwd( R'(C:\Users\James.Thorson\Desktop\Git\VAST)' )
 devtools::document()
